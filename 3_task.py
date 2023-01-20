ce98488acc96676e543a31bb48664c4398286432
vika3108 <vika.antonova1997@gmail.com>
2023-01-20 09:00:07 +0300

    Решили задачу 3

---
+++ 3_task.py
@@ -0,0 +1,7 @@
+# Напишите программу, удаляющую из текста все слова, содержащие "абв"
+text = input('Введите искомые символы: ')
+with open('Text_3_task.txt', 'r', encoding = 'utf_8') as file:
+    # list = [i for i in file.read().split() if text not in i]
+    list = list(filter(lambda x: text not in x, file.read().split()))
+
+print (list)
